aca6295a169469fe16d2db5828baed4dabfbf87a
JimmyYang20 <yangjin39@huawei.com>
2022-07-01 10:20:07 +0800

    Optimize the code based on pylint detection
    1. add code comment.
    2. clean code
    
    Signed-off-by: JimmyYang20 <yangjin39@huawei.com>

---
+++ core/testcasecontroller/algorithm/paradigm/base.py
@@ -0,0 +1,136 @@
+# Copyright 2022 The KubeEdge Authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Paradigm Base"""
+
+import os
+
+from sedna.core.incremental_learning import IncrementalLearning
+
+<<<<<<< HEAD
+from core.common.constant import ModuleType, ParadigmType
+=======
+from core.common.constant import ModuleKind, ParadigmKind
+>>>>>>> c93791e (Optimize the code based on pylint detection)
+
+
+class ParadigmBase:
+    """
+    Paradigm Base
+<<<<<<< HEAD
+    Notes:
+          1. Ianvs serves as testing tools for test objects, e.g., algorithms.
+          2. Ianvs does NOT include code directly on test object.
+          3. Algorithms serve as typical test objects in Ianvs
+          and detailed algorithms are thus NOT included in this Ianvs python file.
+          4. As for the details of example test objects, e.g., algorithms,
+          please refer to third party packages in Ianvs example.
+          For example, AI workflow and interface pls refer to sedna
+          (sedna docs: https://sedna.readthedocs.io/en/latest/api/lib/index.html),
+          and module implementation pls refer to `examples' test algorithms`,
+          e.g., basemodel.py, hard_example_mining.py.
+=======
+>>>>>>> c93791e (Optimize the code based on pylint detection)
+
+    Parameters
+    ---------
+    workspace: string
+        the output required for test process of AI algorithm paradigm.
+    kwargs: dict
+        config required for the test process of AI algorithm paradigm,
+        e.g.: algorithm modules, dataset, etc.
+
+    """
+
+    def __init__(self, workspace, **kwargs):
+        self.modules = kwargs.get("modules")
+        self.dataset = kwargs.get("dataset")
+        self.workspace = workspace
+        self.system_metric_info = {}
+        self.modules_funcs = self._get_module_funcs()
+        os.environ["LOCAL_TEST"] = "TRUE"
+
+    def dataset_output_dir(self):
+        """
+        get output dir of dataset in test process
+
+        Returns
+        ------
+        str
+
+        """
+        output_dir = os.path.join(self.workspace, "dataset")
+        if not os.path.exists(output_dir):
+            os.makedirs(output_dir)
+        return output_dir
+
+    def _get_module_funcs(self):
+        module_funcs = {}
+<<<<<<< HEAD
+        for module_type, module in self.modules.items():
+            func = module.get_module_func(module_type)
+            if callable(func):
+                module_funcs.update({module_type: func})
+        return module_funcs
+
+    def build_paradigm_job(self, paradigm_type):
+        """
+        build paradigm job instance according to paradigm type.
+=======
+        for module_kind, module in self.modules.items():
+            func = module.get_module_func(module_kind)
+            if callable(func):
+                module_funcs.update({module_kind: func})
+        return module_funcs
+
+    def build_paradigm_job(self, paradigm_kind):
+        """
+        build paradigm job instance according to paradigm kind.
+>>>>>>> c93791e (Optimize the code based on pylint detection)
+        this job instance provides the test flow of some algorithm modules.
+
+        Parameters
+        ---------
+<<<<<<< HEAD
+        paradigm_type: str
+=======
+        paradigm_kind: str
+>>>>>>> c93791e (Optimize the code based on pylint detection)
+
+        Returns
+        -------
+        instance
+
+        """
+<<<<<<< HEAD
+        if paradigm_type == ParadigmType.SINGLE_TASK_LEARNING.value:
+            return self.modules_funcs.get(ModuleType.BASEMODEL.value)()
+
+        if paradigm_type == ParadigmType.INCREMENTAL_LEARNING.value:
+            return IncrementalLearning(
+                estimator=self.modules_funcs.get(ModuleType.BASEMODEL.value)(),
+                hard_example_mining=self.modules_funcs.get(
+                    ModuleType.HARD_EXAMPLE_MINING.value)())
+=======
+        if paradigm_kind == ParadigmKind.SINGLE_TASK_LEARNING.value:
+            return self.modules_funcs.get(ModuleKind.BASEMODEL.value)()
+
+        if paradigm_kind == ParadigmKind.INCREMENTAL_LEARNING.value:
+            return IncrementalLearning(
+                estimator=self.modules_funcs.get(ModuleKind.BASEMODEL.value)(),
+                hard_example_mining=self.modules_funcs.get(
+                    ModuleKind.HARD_EXAMPLE_MINING.value)())
+>>>>>>> c93791e (Optimize the code based on pylint detection)
+
+        return None
